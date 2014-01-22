9d63af0fe66bd2d2704a6ac07665bdfcb1d956e7
sirramongabriel <sirramongabriel@gmail.com>
2014-01-22 11:15:23 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'pg'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+	gem 'rspec-rails'
+	gem 'factory_girl_rails'
+end
+
+group :test do
+	gem 'faker'
+	gem 'database_cleaner'
+	gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchPanoply::Application.config.secret_token = 'e7ff2fd4079b7d72bcfaeeeb7696bffe1b294df371fad4b345045ae99bea201e43b9baa811f940a5e705661094f018ba101dcabc4a0212aabe6de04480f94840'
