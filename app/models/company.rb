419a3f68e8503486b2fc5e752bf126f579eb034b
sirramongabriel <sirramongabriel@gmail.com>
2014-01-23 19:29:46 -0500

    Correct factories in company_spec
    Forgot to add Venue.rb and Vote.rb models

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,16 +1,16 @@
 class Company < ActiveRecord::Base
   attr_accessible :address, :city, :name, :phone, :state, :zip
 
-  # validates_presence_of :name, :address, :city, :state, :zip, :phone
+  validates_presence_of :name, :address, :city, :state, :zip, :phone
 
-  # validates_length_of :state, is: 2, too_long: 'please enter a two character state abbreviation',
-  # 										too_short: 'please enter a two character state abreviation'
+  validates_length_of :state, is: 2, too_long: 'please enter a two character state abbreviation',
+  										too_short: 'please enter a two character state abreviation'
 
-  # validates_length_of :zip, within: 5..9, too_long: 'please enter no more than 9 characters',
-  #                     too_short: 'please enter at least 5 characters'
+  validates_length_of :zip, within: 5..9, too_long: 'please enter no more than 9 characters',
+                      too_short: 'please enter at least 5 characters'
 
-  # validates_length_of :phone, is: 10, too_long: 'please enter no more than 10 digits', 
-  #										too_short: 'please enter a 10 digit number'
+  validates_length_of :phone, is: 10, too_long: 'please enter no more than 10 digits', 
+  										too_short: 'please enter a 10 digit number'
 
   has_many :employees
 
--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
 		c.address 	{ Faker::Address.street_address(include_secondary = false) }
 		c.city      { Faker::Address.city }
 		c.state 		{ Faker::Address.state_abbr }
-		c.zip 			{ Faker::Address.zip_code }
-		c.phone 		{ Faker::PhoneNumber.phone_number }
+		c.zip 			'55555'
+		c.phone 		'8885551111'
 	end
 end
