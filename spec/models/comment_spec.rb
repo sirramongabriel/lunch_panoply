a2cf8816490d5b6d2f5a4ba51cbb5a3f3a028a0e
sirramongabriel <sirramongabriel@gmail.com>
2014-01-30 12:35:36 -0500

    Complete comment_spec refactor

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Comment do
+  let(:comment) { create :comment }
+
 	it { should belong_to(:employee) }
 
   it { should belong_to(:menu_item) }
@@ -14,13 +16,11 @@ describe Comment do
 
   context 'with invalid credentials' do
   	it 'displays an error when #title is nil' do
-  		comment = create :comment
   		comment.title = nil
   		expect(comment).to have(1).error_on(:title)
   	end
 
   	it 'displays an error when #content is nil' do
-  		comment = create :comment
   		comment.content = nil
   		expect(comment).to have(1).error_on(:content)
   	end
