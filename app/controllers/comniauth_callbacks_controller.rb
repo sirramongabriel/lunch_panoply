1dcd77e056c16cf7dd36af10765b5487fb6dbde7
sirramongabriel <sirramongabriel@gmail.com>
2014-01-30 16:58:33 -0500

    Troubleshoot omniauth

--- app/controllers/comniauth_callbacks_controller.rb
+++ app/controllers/comniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
-class ComniauthCallbacksController < Devise::OmniauthCallbacksController   
+class ComniauthCallbacksController < ApplicationController
 	def facebook     
-     @employee = Employee.find_for_facebook_oauth(request.env["omniauth.auth"], current_employee)      
-     if @employee.persisted?       
-      sign_in_and_redirect @employee, :event => :authentication #this will throw if @employee is not activated
+     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)      
+     if @user.persisted?       
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_employee_registration_url
+      redirect_to new_user_registration_url
     end
   end
-end
 end
