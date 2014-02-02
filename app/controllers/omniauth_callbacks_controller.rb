f1688ba5c617ba472f3395aeba604bedfcda70fd
sirramongabriel <sirramongabriel@gmail.com>
2014-02-02 16:19:20 -0500

    Troubleshoot omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,15 +2,15 @@ class OmniauthCallbacksController < ApplicationController
 	def facebook     
      @employee = Employee.find_for_facebook_oauth(request.env["omniauth.auth"], current_employee)      
      if @employee.persisted?       
-      sign_in_and_redirect @employee, :event => :authentication #this will throw if @employee is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in_and_redirect(@employee, event: :authentication) #this will throw if @employee is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_employee_registration_path
+      redirect_to(new_employee_registration_path)
     end
   end
 
   def failure
-    render text: 'params[:message]'
+    render text: 'Something went wrong'  # 'params[:message]'
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'popup'
 end
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-# 	provider :identity, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :identity, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
+end
