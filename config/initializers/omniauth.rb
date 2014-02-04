32f6889b39f567235fdb6865cc96485a1e82c9b6
sirramongabriel <sirramongabriel@gmail.com>
2014-02-04 17:10:30 -0500

    Troubleshoot omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-# 	provider :developer unless Rails.env.production?
-#   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'popup'
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :developer unless Rails.env.production?
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'popup'
+end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
 # 	provider :identity, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
