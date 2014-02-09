cc84b0bf20a35eabdc3ff373d8f4b2f4c0718f8a
sirramongabriel <sirramongabriel@gmail.com>
2014-02-09 18:04:34 -0500

    Complete Sign_in view

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
-class Employee < ActiveRecord::Base
+ class Employee < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
