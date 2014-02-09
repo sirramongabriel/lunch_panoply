27370ba24dab39d4161e06250b3033ae32e669f2
sirramongabriel <sirramongabriel@gmail.com>
2014-02-08 20:53:18 -0500

    Precompile assets

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,18 +1,17 @@
 class EmployeesController < ApplicationController
   before_filter :authenticate_employee!, except: [:new, :create]
   before_filter :correct_employee?, except: [:new, :create]
-  before_filter :get_company
 
   def index
-    @employee = @company.employees
+    @employees = Employee.all
   end
 
   def new
-    @employee = @company.employees.build
+    @employee = Employee.new
   end
 
   def create
-    @employee = @company.employees.build(params[:employee])
+    @employee = Employee.new(params[:employee])
     if @employee.save
       redirect_to(@employee, success: 'Employee created!')
     else
@@ -21,15 +20,15 @@ class EmployeesController < ApplicationController
   end
 
   def show
-    @employee = @company.employees.find(params[:id])
+    @employee = Employee.find(params[:id])
   end
 
   def edit
-    @employee = @company.employees.find(params[:id])
+    @employee = Employee.find(params[:id])
   end
 
   def update 
-    @employee = @company.employees.find(params[:id])
+    @employee = Employee.find(params[:id])
     if @employee.update_attributes(params[:employee])
       redirect_to([@company, @employee], success: 'Employee updated!')
     else
@@ -38,14 +37,9 @@ class EmployeesController < ApplicationController
   end
 
   def destroy
-    @employee = @company.employees.find(params[:id])
+    @employee = Employee.find(params[:id])
     @employee.delete
     flash.now[:notice] = 'You sure?'
     redirect_to(company_employees_path(@company))
-  end
-
-  private
-  def get_company
-    @company = Company.find(params[:company_id])
   end
 end
