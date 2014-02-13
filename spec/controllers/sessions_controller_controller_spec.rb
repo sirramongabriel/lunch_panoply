8f8faddb3ceced8a6ce15de8928d4a5cf198e7bd
sirramongabriel <sirramongabriel@gmail.com>
2014-02-12 19:30:14 -0500

    End of day commit

--- spec/controllers/sessions_controller_controller_spec.rb
+++ spec/controllers/sessions_controller_controller_spec.rb
@@ -2,32 +2,24 @@ require 'spec_helper'
 
 describe SessionsControllerController do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
+  # describe 'GET #new' do
+  #   it 'returns http success' do
+  #     get :new
+  #     response.should be_success
+  #   end
+  # end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
+  # describe 'POST :create' do
+  #   it 'returns http success' do
+  #     post :create
+  #     response.should be_success
+  #   end
+  # end
 
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'failure'" do
-    it "returns http success" do
-      get 'failure'
-      response.should be_success
-    end
-  end
-
+  # describe 'GET #destroy' do
+  #   it 'deletes session from the database' do
+  #     delete :destroy
+  #     response.should be_success
+  #   end
+  # end
 end
