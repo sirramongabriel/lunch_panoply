4c7820e28714249120aded5b4142b639817b1513
sirramongabriel <sirramongabriel@gmail.com>
2014-02-18 12:05:42 -0500

    Add location for apis

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,12 @@
+require 'net/http'
+require 'uri'
+
 class MenuItemsController < ApplicationController
   def index
+    uri = URI.parse('devapi.zesty.com')
+    response = http.request_head('devapi.zesty.com')
+
+    respond_with
   end
 
   def create
