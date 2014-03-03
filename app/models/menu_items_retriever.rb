95040c2c5780294dbb65365f1d4818ce8e986bbd
sirramongabriel <sirramongabriel@gmail.com>
2014-03-03 18:16:13 -0500

    End of day commit

--- app/models/menu_items_retriever.rb
+++ app/models/menu_items_retriever.rb
@@ -37,7 +37,7 @@ class MenuItemsRetriever < ActiveRecord::Base
                             { 'Accept' => "application/json; version=2", 'X-HASTY-API-KEY' => ENV['X-HASTY-API-KEY'] }
                          )
     data = JSON.parse("#{request.body}", opts = { symbolize_names: true })
-    data[:dishes][:gluten_free][:true]
+    data[:dishes][:gluten_free]
   end
 
   def hi_protein
