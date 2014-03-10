1c3bd009431c19b64f3f30cd27f87a682eb5e6b1
sirramongabriel <sirramongabriel@gmail.com>
2014-03-10 12:35:24 -0400

    Add menu_items_controller_spec skeletons

--- spec/controllers/menu_items_controller_spec.rb
+++ spec/controllers/menu_items_controller_spec.rb
@@ -6,28 +6,49 @@ describe MenuItemsController do
   describe 'GET #index' do
     it 'returns http success and renders the :index template' do
       get :index, venue_id: menu_item.venue_id, id: menu_item.id
+      expect(response).to render_template :index
       response.should be_success
+    end
+
+    it 'populattes an array of venues' do
+
     end
   end
 
   describe 'GET #create' do
-    it 'returns http success' do
-      get :create
-      response.should be_success
+    context 'given valid credentials' do  
+      it 'redirects to the :show template' do
+        get :create
+        response.should be_success
+      end
+    end
+
+    context 'given invalid credentials' do
+      it 'returns http client error' do
+
+      end
+
+      it 'renders the :new template' do
+
+      end
     end
   end
 
   describe 'GET #show' do
-    it 'returns http success' do
+    it 'returns http success and renders the :edit template' do
       get :show, venue_id: menu_item.venue_id, id: menu_item.id
+      expect(response).to render_template :show
       response.should be_success
     end
   end
 
   describe 'GET #destroy' do
-    it 'returns http success' do
+    it 'deletes an employee from the database' do
       get :destroy, venue_id: menu_item.venue_id, id: menu_item.id
       response.should be_success
+    end
+
+    it 'redirects to the :index template' do
     end
   end
 
